f42151d64c8cfe64465adca751de6a1751b381ce
at-nhanphan <naunemno1@gmail.com>
2017-03-22 11:59:34 +0700

    init

---
+++ app/src/main/java/com/example/naunem/firstproject/activities/FragmentDemoActivity.java
@@ -0,0 +1,8 @@
+package com.example.naunem.firstproject.activities;
+
+/**
+ * Created by naunem on 22/03/2017.
+ */
+
+public class FragmentDemoActivity {
+}
---
+++ app/src/main/java/com/example/naunem/firstproject/fragments/ListUserFragment.java
@@ -0,0 +1,11 @@
+package com.example.naunem.firstproject;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by naunem on 22/03/2017.
+ */
+
+public class ListUserFragment extends FragmentActivity {
+    onc
+}
